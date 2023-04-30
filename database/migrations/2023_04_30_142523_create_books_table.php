487a51ecf1606fd3dcbfe89d2b577b626559a1b5
Yustina Yasin <yustinayasin@gmail.com>
2023-05-01 04:12:52 +0700

    add migrations

--- database/migrations/2023_04_30_142523_create_books_table.php
+++ database/migrations/2023_04_30_142523_create_books_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->longText("alamat");
-            $table->rememberToken();
+            $table->string("name");
+            $table->longText("summary");
+            $table->unsignedBigInteger("id_category");
+
+            $table->foreign("id_category")->references("id")->on("categories");
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
--- database/migrations/2023_04_30_205617_create_authorities_users_table.php
+++ database/migrations/2023_04_30_205617_create_authorities_users_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->longText("alamat");
-            $table->rememberToken();
+        Schema::create('authorities_users', function (Blueprint $table) {
+            $table->unsignedBigInteger("id_authority");
+            $table->unsignedBigInteger("id_user");
+
+            $table->foreign("id_authority")->references("id")->on("authorities");
+            $table->foreign("id_user")->references("id")->on("users");
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('authorities_users');
     }
 };
